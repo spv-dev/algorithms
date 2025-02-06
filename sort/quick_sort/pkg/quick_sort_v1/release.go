b105b3984ea3b405032105451a43d55bee60f56b
Pavel <psjob@bk.ru>
2025-02-06 15:47:18 +0300

    Quick sort random pivot

--- sort/quick_sort/pkg/quick_sort_v1/release.go
+++ sort/quick_sort/pkg/quick_sort_v1/release.go
@@ -1,13 +1,21 @@
 package quicksort
 
-func subArrays(nums []int, pivot int) ([]int, []int) {
+import (
+	"math/rand"
+)
+
+func subArrays(nums []int, pivotIndex int) ([]int, []int) {
+	pivot := nums[pivotIndex]
+
 	less := make([]int, 0, len(nums)/2)
 	greater := make([]int, 0, len(nums)/2)
 	for i := 0; i < len(nums); i++ {
-		if nums[i] < pivot {
-			less = append(less, nums[i])
-		} else {
-			greater = append(greater, nums[i])
+		if i != pivotIndex {
+			if nums[i] < pivot {
+				less = append(less, nums[i])
+			} else {
+				greater = append(greater, nums[i])
+			}
 		}
 	}
 
@@ -19,9 +27,17 @@ func Sort(nums []int) []int {
 		return nums
 	}
 
-	pivot := nums[0]
+	if len(nums) == 2 {
+		if nums[0] > nums[1] {
+			return []int{nums[1], nums[0]}
+		} else {
+			return nums
+		}
+	}
 
-	less, greater := subArrays(nums[1:], pivot)
+	pivotIndex := rand.Intn(len(nums))
 
-	return append(append(Sort(less), pivot), Sort(greater)...)
+	less, greater := subArrays(nums, pivotIndex)
+
+	return append(append(Sort(less), nums[pivotIndex]), Sort(greater)...)
 }
