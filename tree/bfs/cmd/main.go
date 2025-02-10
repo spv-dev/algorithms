e30887a6306afeafc94f6834c4865803eecf6986
Pavel <psjob@bk.ru>
2025-02-10 16:26:34 +0300

    BFS

---
+++ tree/bfs/cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	bfs "github.com/spv-dev/algorithms/tree/bfs/pkg/bfs_v1"
+)
+
+func main() {
+	friends := map[string][]string{
+		"I":      {"Alice", "Bob", "Clair"},
+		"Bob":    {"Anudj", "Peggi"},
+		"Alice":  {"Peggi"},
+		"Clair":  {"Tomas", "Johnny"},
+		"Anudj":  {},
+		"Peggi":  {},
+		"Tom":    {},
+		"Johnny": {"Mark"},
+		"Mark":   {"Helen", "Johnny", "Aram"},
+		"Helen":  {},
+		"Aram":   {},
+	}
+
+	fmt.Println(bfs.HasPath(friends, "I", "Aram"))
+
+	city_map := map[string][]string{
+		"Home":     {"Park", "School", "Mail"},
+		"Park":     {"Home", "Museum", "Cafe"},
+		"School":   {"Home", "Library", "Mail"},
+		"Mail":     {"Home", "School", "Hospital"},
+		"Library":  {"School", "Hospital"},
+		"Hospital": {"Library", "Mail", "Office"},
+		"Cafe":     {"Park", "Theater", "Office"},
+		"Museum":   {"Park", "Shop"},
+		"Shop":     {"Museum", "Theater"},
+		"Theater":  {"Shop", "Cafe"},
+		"Office":   {"Cafe", "Hospital"},
+	}
+	fmt.Println(bfs.HasPath(city_map, "Home", "Theater"))
+
+}
